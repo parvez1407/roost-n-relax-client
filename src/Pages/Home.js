7506e754ea5ac0642aa76878f0ba9448f43412a0
parvez1407 <info.nahidwd@gmail.com>
2023-02-20 02:18:10 +0600

    implement search form in home page and install Headless UI

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import SearchForm from '../Components/Form/SearchForm';
 import Spinner from '../Components/Spinner/Spinner'
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allExp, setAllExp] = useState([]);
+
+  useEffect(() => {
+    fetch('expdata.json')
+      .then(res => res.json())
+      .then(data => {
+        setAllExp(data)
+      })
+  }, [])
+
   return (
-    <>
-      <Spinner />
-    </>
+    <div className='md:flex justify-center gap-10 px-6 md:px-10 lg:px-20'>
+
+      <div>
+        <SearchForm />
+      </div>
+
+      <div>
+        <div>Home Cares</div>
+        <div>Experience Cards</div>
+      </div>
+
+    </div>
   )
 }
 
